18462efc4b277432f267abdf2dce8a761c98dbcc
Mateo Neglia <mateo.neglia@gmail.com>
2023-01-26 15:22:27 -0300

    first commit

---
+++ src/Palette/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ColorBox from '../ColorBox/ColorBox';
+import './Palette.css';
+
+export default class Palette extends Component {
+    render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background = {color.color} name={color.name}/>
+        ))
+        return(
+            <div className="palette">
+                <div className="palette-colors">{colorBoxes}</div>
+            </div>
+        );
+    }
+}
